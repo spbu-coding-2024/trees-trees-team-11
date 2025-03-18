15d854804d20339d12de6d216abe02330cffb89a
vlapugb <vladimirpugovkinspb@gmail.com>
2025-03-18 17:18:09 +0300

    add: clean, iterator, constainsKey func's

--- lib/src/main/kotlin/LibTree/interfaceTree/Tree.kt
+++ lib/src/main/kotlin/LibTree/interfaceTree/Tree.kt
@@ -1,4 +1,9 @@
 package LibTree.interfaceTree
 
-interface Tree {
+interface Tree<K : Comparable<K>,V, root> : Iterable<Pair<K,V>> {
+    fun height(): Int
+    fun containsKey(key: K): Boolean
+    fun erase(key: K)
+    fun insert(key: K, value: V)
+    fun clean()
 }
