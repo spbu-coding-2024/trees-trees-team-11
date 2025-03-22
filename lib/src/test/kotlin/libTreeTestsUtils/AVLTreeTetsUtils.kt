297ad038e2cc68e5453d6c0bbc11d5d267a5d804
vlapugb <vladimirpugovkinspb@gmail.com>
2025-03-22 12:33:32 +0300

    add: utils for tests

--- lib/src/test/kotlin/libTreeTestsUtils/AVLTreeTetsUtils.kt
+++ lib/src/test/kotlin/libTreeTestsUtils/AVLTreeTetsUtils.kt
@@ -16,13 +16,32 @@ class UtilsFunctions {
 
         return is_balanced(root.left) && is_balanced(root.right)
     }
+    fun<K:Comparable<K>, V> nodesInTreeCounter(tree: AVLTree<K, V>?): Int {
+        if(tree == null) return 0
+        val listOfNodes = mutableListOf<K>()
+        for ((key, _) in tree) {
+            listOfNodes.add(key)
+        }
+        val nodesInTree = listOfNodes.size
+        return nodesInTree
+    }
+
+    fun <K: Comparable<K>, V> showKeyValue(root: AVLTree.AVLNode<K, V>?, key: K): AVLTree.AVLNode<K, V> {
+        return when {
+            root == null -> throw NoSuchElementException("Key $key not found")
+            key < root.key -> showKeyValue(root.left, key)
+            key > root.key -> showKeyValue(root.right, key)
+            else -> root
+        }
+    }
+
+
+
 
     private fun<K : Comparable<K>,V> height(node: AVLTree.AVLNode<K, V>?) : Int {
         if(node == null) return 0
         return 1 + max(height(node.left), height(node.right))
     }
-
-
 
 
 
