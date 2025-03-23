12d0bf9aea83898cc2e440d91e5ee670a8b6d651
ganesha <tatiana.gromova2022@yandex.ru>
2025-03-24 00:25:48 +0300

    refactor: remove redundant comments

--- lib/src/main/kotlin/libTree/trees/BSTree.kt
+++ lib/src/main/kotlin/libTree/trees/BSTree.kt
@@ -4,28 +4,11 @@ import libTree.interfaceTree.Tree
 
 /**
  * Implementation of a Binary Search Tree (BST)
- *
- * A BST is a binary tree that satisfies the following additional conditions (search tree
- * properties):
- * - Both the left and right subtrees are binary search trees
- * - For any node X, all keys in the left subtree of X are less than or equal to the key of X
- * - For any node X, all keys in the right subtree of X are greater than the key of X
- *
- * @param K The type of the node's key, which must support the "less than" comparison operation
- * @param V The payload (value) associated with the key
  */
 class BSTree<K : Comparable<K>, V>(
     private var root: BSNode<K, V>? = null,
 ) : Tree<K, V, BSTree.BSNode<K, V>> {
 
-    /**
-     * Represents a node in the Binary Search Tree (BST) in memory
-     *
-     * @param key The key of the node
-     * @param value The value associated with the key
-     * @param left The left child of the node
-     * @param right The right child of the node
-     */
     class BSNode<K : Comparable<K>, V>(
         key: K,
         value: V,
@@ -37,15 +20,11 @@ class BSTree<K : Comparable<K>, V>(
 
     // ======================== Tree Information ========================
 
-    /** Returns the height of the tree using recursive approach */
+    /**
+     * Returns the height of the tree using recursive approach
+     */
     override fun height(): Int = heightRecursive(root)
 
-    /**
-     * Recursive function to calculate the height of the given binary tree
-     *
-     * @param root The root of the subtree
-     * @return The height of the tree
-     */
     private fun heightRecursive(root: BSNode<K, V>?): Int {
         return if (root == null) {
             0
@@ -54,21 +33,11 @@ class BSTree<K : Comparable<K>, V>(
         }
     }
 
-    /**
-     * Checks if the tree is empty
-     *
-     * @return `true` if the tree is empty, otherwise `false`
-     */
     fun isTreeEmpty(): Boolean = root == null
 
-    /**
-     * Checks if the tree is not empty
-     *
-     * @return `true` if the tree is not empty, otherwise `false`
-     */
+
     fun isTreeNotEmpty(): Boolean = root != null
 
-    /** Clears the tree by removing all of its nodes */
     override fun clean() {
         root = null
     }
@@ -77,17 +46,11 @@ class BSTree<K : Comparable<K>, V>(
 
     /**
      * Checks if the tree contains a key
-     *
-     * @param key The key to search for
-     * @return `true` if the key exists in the tree, otherwise `false`
      */
     override fun containsKey(key: K): Boolean = getValue(key) != null
 
     /**
      * Searches for a value by key
-     *
-     * @param key The key to search for
-     * @return The value associated with the key, or `null` if the key is not found
      */
     fun getValue(key: K): V? {
         var current = root
@@ -109,9 +72,6 @@ class BSTree<K : Comparable<K>, V>(
      *
      * If the key already exists in the tree, the duplicate counter is incremented If the key is not
      * found, a new node is created and added to the tree
-     *
-     * @param key The key to insert
-     * @param value The value associated with the key
      */
     override fun insert(key: K, value: V) {
         root = insertRecursive(root, key, value)
@@ -119,10 +79,6 @@ class BSTree<K : Comparable<K>, V>(
 
     /**
      * Recursive helper function for inserting a key-value pair into the tree
-     *
-     * @param node The current node in the recursion
-     * @param key The key to insert
-     * @param value The value associated with the key
      * @return The updated node after insertion
      */
     private fun insertRecursive(node: BSNode<K, V>?, key: K, value: V): BSNode<K, V> {
@@ -149,8 +105,6 @@ class BSTree<K : Comparable<K>, V>(
      *
      * If the key has duplicates, the duplicate count is decremented If the key has no duplicates,
      * the node is removed from the tree
-     *
-     * @param key The key to remove
      */
     override fun erase(key: K) {
         root = deleteRecursive(root, key)
@@ -158,9 +112,6 @@ class BSTree<K : Comparable<K>, V>(
 
     /**
      * Recursive helper function for deleting a node with the specified key
-     *
-     * @param node The current node in the recursion
-     * @param key The key to delete
      * @return The updated node after deletion
      */
     private fun deleteRecursive(node: BSNode<K, V>?, key: K): BSNode<K, V>? {
@@ -199,7 +150,9 @@ class BSTree<K : Comparable<K>, V>(
 
     // ======================== Traversals ========================
 
-    /** Returns an iterator for traversing the tree in Inorder (left -> root -> right) */
+    /**
+     * Returns an iterator for traversing the tree in Inorder (left -> root -> right)
+     * */
     override fun iterator(): Iterator<Pair<K, V>> = inorder().iterator()
 
     enum class TraversalType {
@@ -221,17 +174,14 @@ class BSTree<K : Comparable<K>, V>(
             TraversalType.POSTORDER -> postorder()
         }
 
-    /** Returns a list of key-value pairs for Inorder traversal (left -> root -> right) */
     fun inorder(): List<Pair<K, V>> {
         return traverse(root, TraversalType.INORDER)
     }
 
-    /** Returns a list of key-value pairs for Preorder traversal (root -> left -> right) */
     fun preorder(): List<Pair<K, V>> {
         return traverse(root, TraversalType.PREORDER)
     }
 
-    /** Returns a list of key-value pairs for Postorder traversal (left -> right -> root) */
     fun postorder(): List<Pair<K, V>> {
         return traverse(root, TraversalType.POSTORDER)
     }
@@ -260,9 +210,6 @@ class BSTree<K : Comparable<K>, V>(
 
     /**
      * Returns the node with the smallest key in the given subtree
-     *
-     * @param node The root of the subtree
-     * @return The node with the minimum key, or `null` if the subtree is empty
      */
     private fun minValueNode(node: BSNode<K, V>?): BSNode<K, V>? {
         var current = node
@@ -274,9 +221,6 @@ class BSTree<K : Comparable<K>, V>(
 
     /**
      * Returns the node with the largest key in the given subtree
-     *
-     * @param node The root of the subtree
-     * @return The node with the maximum key, or `null` if the subtree is empty
      */
     private fun maxValueNode(node: BSNode<K, V>?): BSNode<K, V>? {
         var current = node
