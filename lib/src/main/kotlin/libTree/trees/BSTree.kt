8b67fbac7bc3ddae93271e1539ce6929e2adfb0c
ganesha <tatiana.gromova2022@yandex.ru>
2025-03-27 20:28:01 +0300

    refactor: changed iterator, added constructor and renamed function

--- lib/src/main/kotlin/libTree/trees/BSTree.kt
+++ lib/src/main/kotlin/libTree/trees/BSTree.kt
@@ -5,7 +5,7 @@ import libTree.interfaceTree.Tree
 /**
  * Implementation of a Binary Search Tree (BST)
  */
-class BSTree<K : Comparable<K>, V>(
+class BSTree<K : Comparable<K>, V> private constructor(
     private var root: BSNode<K, V>? = null,
 ) : Tree<K, V, BSTree.BSNode<K, V>> {
 
@@ -14,16 +14,17 @@ class BSTree<K : Comparable<K>, V>(
         value: V,
         public override var left: BSNode<K, V>? = null,
         public override var right: BSNode<K, V>? = null,
-    ) : BaseNode<K, V, BSNode<K, V>>(key, value, left, right, 1)
+        public override var height: Long = 1,
+    ) : BaseNode<K, V, BSNode<K, V>>(key, value, left, right, height)
+
+    constructor() : this(null)
 
     // ======================== Tree Methods ========================
 
     /**
      * Returns the height of the tree using recursive approach
      */
-    override fun height(): Int {
-        return heightOfTree(root)
-    }
+    override fun height(): Int = heightOfTree(root)
 
     private fun heightOfTree(root: BSNode<K, V>?): Int {
         if (root == null) {
@@ -45,9 +46,7 @@ class BSTree<K : Comparable<K>, V>(
     /**
      * Checks if the tree contains a key
      */
-    override fun containsKey(key: K): Boolean {
-        return findNode(key) != null
-    }
+    override fun containsKey(key: K): Boolean = findNode(key) != null
 
     /**
      * Finds and returns the node with the specified key using iterative search
@@ -61,12 +60,11 @@ class BSTree<K : Comparable<K>, V>(
                 return current
             }
 
-            current =
-                if (current.key > key) {
-                    current.left
-                } else {
-                    current.right
-                }
+            current = if (current.key > key) {
+                current.left
+            } else {
+                current.right
+            }
         }
 
         return null
@@ -134,24 +132,21 @@ class BSTree<K : Comparable<K>, V>(
                 return node.left
             }
 
-            val successor: BSNode<K, V>? = minNode(node, key)
+            val successor: BSNode<K, V>? = minValueNode(node)
             if (successor != null) {
                 node.key = successor.key
                 node.right = deleteNode(node.right, successor.key)
             }
         }
+
         return node
     }
 
     /**
      * Returns the node with the minimum key in the given subtree
      */
-    private fun minNode(node: BSNode<K, V>?, key: K): BSNode<K, V>? {
+    private fun minValueNode(node: BSNode<K, V>?): BSNode<K, V>? {
         var current: BSNode<K, V>? = node
-        if (current != null) {
-            current = current.right
-        }
-
         while (current?.left != null) {
             current = current.left
         }
@@ -165,33 +160,18 @@ class BSTree<K : Comparable<K>, V>(
      * Returns an iterator for traversing the tree in Inorder (left -> root -> right)
      */
     override fun iterator(): Iterator<Pair<K, V>> {
-        return TreeIterator(root)
-    }
-
-    /**
-     * Inorder iterator implementation for BST
-     */
-    private class TreeIterator<K : Comparable<K>, V>(root: BSNode<K, V>?) : Iterator<Pair<K, V>> {
-        private val stack: MutableList<BSNode<K, V>> = mutableListOf()
-        private var current: BSNode<K, V>? = root
-
-        override fun next(): Pair<K, V> {
-            while (current != null) {
-                val currentNotNull = current
-                if (currentNotNull != null) {
-                    stack.add(currentNotNull)
-                    current = currentNotNull.left
-                }
+        val list = mutableListOf<Pair<K, V>>()
+        fun treeIterator(node: BSNode<K, V>?) {
+            if (node == null) {
+                return
             }
 
-            val node = stack.removeAt(stack.size - 1)
-            current = node.right
-
-            return node.key to node.value
+            treeIterator(node.left)
+            list.add(node.key to node.value)
+            treeIterator(node.right)
         }
 
-        override fun hasNext(): Boolean {
-            return (stack.isNotEmpty() || current != null)
-        }
+        treeIterator(root)
+        return list.iterator()
     }
 }
